b064ecc60dcfb1f041ad6b66da490242f3c6c692
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-05 18:53:22 -0500

    Create exiting.py

---
+++ exiting.py
@@ -0,0 +1,14 @@
+import colorama
+from colorama import Fore, Style
+import time
+import timeout_decorator
+# Making the "Exiting.." 
+def exit_server_msg():
+  time.sleep(3)
+  print('   Ending Server...')
+  time.sleep(2)
+  print('     Server ended..')
+  time.sleep(3)
+  print('       Exiting..')
+  time.sleep(1)
+  print(Fore.WHITE + '         ᑢ ᓍ ᘻ ᘿ  ᗷᗩᑢᖽᐸ  ᗩ ᘜ ᗩ ᓰ ᘉ  !')
