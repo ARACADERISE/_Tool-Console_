beae90df6ed3f57f0b835ec5f57a3e974a52a6bc
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-05 11:58:39 -0500

    Add files via upload

---
+++ error.py
@@ -0,0 +1,6 @@
+# Making a function error() to print an error
+def error():
+  ERROR_MESSAGE = "Oops..seems like that isn't a choice from the choices above, please try again.."
+  print('----' * 9)
+  print(ERROR_MESSAGE)
+  print('----' * 9)
