0611fd500147a2f1ff47f9f216614ef072542849
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-05 19:46:56 -0500

    Update error.py

--- error.py
+++ error.py
@@ -3,7 +3,7 @@ from colorama import Fore, Style
 import time
 # Making a function error() to print an error
 def error():
-  time.sleep(2)
+  time.sleep(4)
   ERROR_MESSAGE = "Oops..seems like that isn't a choice from the choices above, please try again.."
   print(Fore.RED + '----' * 9)
   print(ERROR_MESSAGE)
