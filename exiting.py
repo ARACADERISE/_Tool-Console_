274d8b6ed818aea8e84e432033802c541dd83707
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-06 16:08:28 -0500

    exits the project

--- exiting.py
+++ exiting.py
@@ -1,13 +1,15 @@
 # import colorama
 from colorama import Fore, Style
 import time
-# Making the "Exiting.." 
-def exit_server_msg():
+
+# ENDS THE SERVER PORT
+def leave_port(p):
   time.sleep(3)
-  print('   Ending Server...')
+  print('   Ending Server Port ' + p + '...')
   time.sleep(2)
-  print('     Server ended..')
+  print('     Server port ' + p + ' ended..')
   time.sleep(3)
   print('       Exiting..')
   time.sleep(1)
   print(Fore.WHITE + '         ᑢ ᓍ ᘻ ᘿ  ᗷᗩᑢᖽᐸ  ᗩ ᘜ ᗩ ᓰ ᘉ  !')
+  
