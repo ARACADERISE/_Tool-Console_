9b89e3c7b8ed59b072084ace4a30741fadbf1bcd
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-05 19:26:29 -0500

    Create load_file.py

---
+++ load_file.py
@@ -0,0 +1,13 @@
+import time
+import colorama
+from colorama import Fore, Style
+def load_():
+  time.sleep(1.5)
+  print(Fore.MAGENTA + 'Loading...')
+  time.sleep(0.5)
+  print('  Getting...')
+  time.sleep(3)
+  print('    Fetching...')
+  time.sleep(1)
+  print('File Loaded...\n\n')
+  time.sleep(1)
