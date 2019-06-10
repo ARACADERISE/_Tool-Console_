24c4976ffde5174aedba7c82efced5bc276ee687
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-10 17:17:42 -0500

    port packages

---
+++ packages.py
@@ -0,0 +1,8 @@
+def pkg_1():
+  pkg = {'Stored': 'As_Default', 'File_Author': None, 'Type': 'Extra'}
+  return pkg
+def pkg_2():
+  pkg = {'Stored': 'As_Default', 'File_Author': None, 'Type': 'Extra'}
+  return pkg
+def def_pkg():
+  pkg = {'Stored': 'As_Default', 'File_Author': 'None', 'Type': 'Default_Extra'}
