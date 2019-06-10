f25d5fe04497506931126e674101885403b77029
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-10 17:13:30 -0500

    loads the project

--- load_file.py
+++ load_file.py
@@ -1,7 +1,7 @@
 import time
 # import colorama
 from colorama import Fore, Style, Back
-from random import randint
+from config import _buffer_
 
 def load_():
 
@@ -61,36 +61,68 @@ def load_():
     ´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´
     ''')
     time.sleep(1)
-    print(Style.RESET_ALL + Fore.GREEN + '_ㄒ  ㄖ  ㄖ  ㄥ  -  匚  ㄖ  几  丂  ㄖ  ㄥ  乇_')
+    print(Style.RESET_ALL + Fore.GREEN + '''
+     ____ __   __  __        ___ __  __ _ ____  __  __   ____ 
+    (_  _/  \ /  \(  )  ___ / __/  \(  ( / ___)/  \(  ) (  __)
+      )((  O (  O / (_/(___( (_(  O /    \___ (  O / (_/\) _) 
+     (__)\__/ \__/\___/     \___\__/\_)__(____/\__/\____(____)''')
     time.sleep(1.5)
-    print('ＭＡＤＥ　ＢＹ：　ＡＲＡＣＡＤＥ_ＲＩＳＥ')
-    print('(version 1.0.1)\n')
+    print('    ＭＡＤＥ　ＢＹ：　ＡＲＡＣＡＤＥ_ＲＩＳＥ')
+    print('    version 1.0.1\n')
     time.sleep(0.5)
     break
 
-def port_l(p):
+def port_l(p, is_live):
+
+  _append_ = []
+  load_info = []
 
   time.sleep(1.4)
   print(Style.RESET_ALL + Fore.GREEN + '\nLoading Port ' + p + '...')
+  load_info.append(['port has started loading'])
+  _append_.append(load_info)
   time.sleep(3.8)
   print('Adapting port ' + p + '...')
+  load_info.append(['port adapting..'])
+  _append_.append(load_info)
   time.sleep(6.5)
   print('Adaptation of port ' + p + ' starting...')
+  load_info.append(['adapting of port started'])
+  _append_.append(load_info)
   time.sleep(4)
   print('Adapting port ' + p + ' to root...')
+  load_info = ['port to root started']
+  _append_.append(load_info)
   time.sleep(9.5)
   print('Adapting port ' + p + ' to local...')
+  load_info.append(['port to local started'])
+  _append_.append(load_info)
   time.sleep(1.8)
-  print(Fore.WHITE + Back.BLUE + Style.BRIGHT + '     This May Take Awhile     ')
+  print(Fore.WHITE + Back.BLUE + Style.BRIGHT + '   This May Take Awhile   ')
   time.sleep(21)
-  print(Style.RESET_ALL + Fore.GREEN + 'Checking status of port ' + p + '(live/not live)...')
+  print(Style.NORMAL + Back.RESET + Fore.GREEN + 'Checking status of port ' + p + '(live/not live)...')
+  load_info.append(['scan of port status started'])
+  _append_.append(load_info)
+  time.sleep(10)
+  if is_live == 'Live':
+    print('Status: LIVE')
+  elif is_live == 'Not Live':
+    print('Status: NOT LIVE')
+  elif is_live == 'Live by default' or is_live == 'Live By Default':
+    print('Status: LIVE BY DEFAULT')
   time.sleep(12)
   print('Done!')
   time.sleep(2)
   print('Please wait a moment while we load your oficial port...')
+  load_info.append(['port being created officially'])
+  _append_.append(load_info)
   time.sleep(5.5)
   print('Port ' + p + ' loaded, you are now server port ' + p)
+  load_info.append(['port has been loaded for user to use'])
+  _append_.append(load_info)
   time.sleep(4.8)
+  for item in _append_:
+    _buffer_(item)
 
 # CORRUPT, NOT USED
 def _port_used_(p):
