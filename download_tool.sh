770c0d35ed72404be3f8afa1e5abe5b3776ac6f2
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 16:29:58 -0500

    Update download_tool.sh

--- download_tool.sh
+++ download_tool.sh
@@ -1,1 +1,27 @@
 
+
+GREEN="\033[92m"
+RED="\033[91m"
+BLUE="\033[94m"
+WHITE="\033[0;37m"
+
+echo -e "$GREEN $RESET\n\n"
+
+echo -e "
+      ___ __   __  __        ___ __  __ _ ____  __  __   ____ 
+    (_  _/  \ /  \(  )  ___ / __/  \(  ( / ___)/  \(  ) (  __)
+      )((  O (  O / (_/(___( (_(  O /    \___ (  O / (_/\) _) 
+     (__)\__/ \__/\___/     \___\__/\_)__(____/\__/\____(____) V.1.0.1
+                   Top Tools, Top Developers, Free,
+                   Tool Download Version
+                   MADE_BY: ARACADE_RISE,
+                   "
+ 
+echo -e "$RED \n--> Choose A Tool <--\n $WHITE"
+
+user = echo -e "$BLUE \n1. Something $WHITE"
+
+if [user == '1']
+then
+  apt update
+  git clone https://github.com/ARACADERISE/_Tool-Console_.git
