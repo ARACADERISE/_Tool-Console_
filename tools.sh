679f3f24b41628dbb7b65419ba158ed2703f6fd8
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 17:03:40 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -21,7 +21,7 @@ echo -e "$RED \n--> Choose A Tool <--\n $WHITE"
 tools() 
 {
   echo -e "$BLUE \n-->TOOLS<--\n "
-  echo -e "$BLUE1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
+  echo -e "$BLUE\n1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
   read -p "Your Choice >> " choice 
 }
 tools
