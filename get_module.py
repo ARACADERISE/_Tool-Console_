184b917b7929ab09ad9bcef1c4012c7252ea7261
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 14:36:18 -0500

    gets project info

--- get_module.py
+++ get_module.py
@@ -1,31 +1,13 @@
 import time
 from colorama import Fore, Style
 
-# No use, but may help us later...better to declare each as false then declare them as true when we print that we've found the data
-https_found = False
-github_found = False
-user_found = False
-filename_found = False
-
-def get_():
-  time.sleep(1)
-  print(Fore.RED + 'Fetching Information...')
-  time.sleep(2)
-  print('Got https')
-  https_found = True
-  time.sleep(1)
-  print('Found https://github.com/')
-  github_found = True
+def get_(proj_name):
+  time.sleep(3)
+  print(Fore.RED + 'Fetching Information For ' + proj_name + '...')
   time.sleep(5)
-  print('Getting github user...')
-  time.sleep(3)
-  print('Found github user')
-  user_found = True
-  time.sleep(1)
-  print('Fetching file name..')
-  time.sleep(4)
-  print('Found file name...')
-  filename_found = True
-  time.sleep(2)
-  print('Please wait.....')
-  time.sleep(3)
+  print('Getting Link For ' + proj_name + '...')
+  time.sleep(6)
+  print('Scanning Steps For ' + proj_name + '....')
+  time.sleep(8)
+  print('One more moment...')
+  time.sleep(3.6)
