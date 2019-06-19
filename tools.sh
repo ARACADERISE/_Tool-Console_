a3ab732b1023792b34f757696f381193ffb61e0c
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 17:44:32 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -24,9 +24,9 @@ tool()
 {
 echo -e "$BLUE \n-->TOOLS<--\n "
 echo -e "$BLUE\n1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
+read -p "Your Choice >> " choice 
 }
 tool
-read -p "Your Choice >> " choice 
 
 if [ $choice == 1 ]
 then
