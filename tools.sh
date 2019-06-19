bd216e42c9e98da4f056460da27d71e924f7b66e
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 17:18:59 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -25,44 +25,43 @@ read -p "Your Choice >> " choice
 choice=1,2,3,4,5,6,7,8,9,10
 run=$false
 
-while [ $run == $false ]
-do
-  if [ $choice == 1 ]
-  then
-    echo -e "$RED \n-->INSTALLING SNIPER<--\n $WHITE"
-    git clone https://github.com/1N3/Sn1per
-    echo -e "$BLUE \n1.cd Sniper\n
-    2.bash install.sh"
-  elif [ $choice == 2 ]
-  then
-    echo -e "$RED \n-->INSTALLING JohnTheRipper<--\n $WHITE"
-    git clone https://github.com/magnumripper/JohnTheRipper.git
-    echo -e "$BLUE \n1.cd JohnTheRipper\n2.cd run"
-  elif [ $choice == 3 ]
-  then
-    echo -e "$RED \n-->INSTALLING HYDRA<--\n $WHITE"
-    pkg install hydra
-  elif [ $choice == 4 ]
-  then
-    echo -e "$RED \n-->INSTALLING METASPLOIT<--\n $WHITE"
-    pkg install unstable-repo
-    pkg install metasploit
-  elif [ $choice == 5 ]
-  then
-    echo -e "$RED \n-->INSTALLING sploitego<--\n $WHITE"
-    echo -e "$WHITE trying to find package..."
-    echo -e "$RED \n-->COULD NOT FIND<--\n"
-    echo -e "try visiting $BLUEhttps://github.com/allfro/sploitego"
-  elif [ $choice == 6 ]
-  then
-    echo -e "$RED \n-->INSTALLING Hunner<--\n $WHITE"
-    git clone https://github.com/b3-v3r/Hunner
-  elif [ $choice == 7 ]
-  then
-    ehco -e "$RED \n-->INSTALLING sqlmap<--\n $WHITE"
-    git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-  elif [ $choice == 8 ]
-  then
-    ehco -e "$RED \n-->INSTALLING shellphish<--\n $WHITE"
-    git clone https://github.com/thelinuxchoice/shellphish
+
+if [ $choice == 1 ]
+then
+  echo -e "$RED \n-->INSTALLING SNIPER<--\n $WHITE"
+  git clone https://github.com/1N3/Sn1per
+  echo -e "$BLUE \n1.cd Sniper\n
+  2.bash install.sh"
+elif [ $choice == 2 ]
+then
+  echo -e "$RED \n-->INSTALLING JohnTheRipper<--\n $WHITE"
+  git clone https://github.com/magnumripper/JohnTheRipper.git
+  echo -e "$BLUE \n1.cd JohnTheRipper\n2.cd run"
+elif [ $choice == 3 ]
+then
+  echo -e "$RED \n-->INSTALLING HYDRA<--\n $WHITE"
+  pkg install hydra
+elif [ $choice == 4 ]
+then
+  echo -e "$RED \n-->INSTALLING METASPLOIT<--\n $WHITE"
+  pkg install unstable-repo
+  pkg install metasploit
+elif [ $choice == 5 ]
+then
+  echo -e "$RED \n-->INSTALLING sploitego<--\n $WHITE"
+  echo -e "$WHITE trying to find package..."
+  echo -e "$RED \n-->COULD NOT FIND<--\n"
+  echo -e "try visiting $BLUEhttps://github.com/allfro/sploitego"
+elif [ $choice == 6 ]
+then
+  echo -e "$RED \n-->INSTALLING Hunner<--\n $WHITE"
+  git clone https://github.com/b3-v3r/Hunner
+elif [ $choice == 7 ]
+then
+  ehco -e "$RED \n-->INSTALLING sqlmap<--\n $WHITE"
+  git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
+elif [ $choice == 8 ]
+then
+  ehco -e "$RED \n-->INSTALLING shellphish<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/shellphish
 fi
