47eafc0bbc51c1b8402e3fa39102fb3f13bc1ec2
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 17:41:18 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -32,47 +32,46 @@ if [ $choice == 1 ]
 then
   echo -e "$RED \n-->INSTALLING SNIPER<--\n $WHITE"
   git clone https://github.com/1N3/Sn1per
-  echo -e "$BLUE \n1.cd Sniper\n
-  2.bash install.sh"
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BLUE \n1.cd Sniper\n2.bash install.sh"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 2 ]
 then
   echo -e "$RED \n-->INSTALLING JohnTheRipper<--\n $WHITE"
   git clone https://github.com/magnumripper/JohnTheRipper.git
   echo -e "$BLUE \n1.cd JohnTheRipper\n2.cd run"
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 3 ]
 then
   echo -e "$RED \n-->INSTALLING HYDRA<--\n $WHITE"
   pkg install hydra
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 4 ]
 then
   echo -e "$RED \n-->INSTALLING METASPLOIT<--\n $WHITE"
   pkg install unstable-repo
   pkg install metasploit
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 5 ]
 then
   echo -e "$RED \n-->INSTALLING sploitego<--\n $WHITE"
   echo -e "$WHITE trying to find package..."
   echo -e "$RED \n-->COULD NOT FIND<--\n"
   echo -e "try visiting $BLUEhttps://github.com/allfro/sploitego"
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 6 ]
 then
   echo -e "$RED \n-->INSTALLING Hunner<--\n $WHITE"
   git clone https://github.com/b3-v3r/Hunner
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 7 ]
 then
   echo -e "$RED \n-->INSTALLING sqlmap<--\n $WHITE"
   git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 8 ]
 then
   ehco -e "$RED \n-->INSTALLING shellphish<--\n $WHITE"
   git clone https://github.com/thelinuxchoice/shellphish
-  echo -e "$BOLDr \n--> I  N  S  T  A  L  L  E  D<--\n"
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 fi
 tool
