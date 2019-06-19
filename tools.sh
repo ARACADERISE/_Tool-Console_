520ac19474651d7b9eb7b1b78553b6cb7fcaffef
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 16:57:53 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -23,6 +23,7 @@ tools()
   echo -d "$BLUE \n-->TOOLS<--\n "
   echo -e "$BLUE 1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
 }
+tools()
 choice=1,2,3,4,5,6,7,8,9,10
 read -p ">> Your Choice >> " choice 
 
