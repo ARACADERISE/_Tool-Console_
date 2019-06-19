05de9c5276a5a483b2ce34bc06ac3ff18f507f5c
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 17:14:25 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -21,12 +21,12 @@ echo -e "
 echo -e "$RED \n--> Choose A Tool <--\n $WHITE"
 tools() 
 {
-  while [ $run == $true ]
+  while ( $run == $true )
   do
     echo -e "$BLUE \n-->TOOLS<--\n "
     echo -e "$BLUE\n1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
     read -p "Your Choice >> " choice 
-  done
+  end
 }
 tools
 choice=1,2,3,4,5,6,7,8,9,10
