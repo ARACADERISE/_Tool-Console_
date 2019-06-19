859a1438b23ff78773502ce397c0703d53847fa6
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 18:33:59 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -43,17 +43,23 @@ then
   git clone https://github.com/magnumripper/JohnTheRipper.git
   echo -e "$BLUE \n1.cd JohnTheRipper\n2.cd run"
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 3 ]
 then
   echo -e "$RED \n-->INSTALLING HYDRA<--\n $WHITE"
   pkg install hydra
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 4 ]
 then
   echo -e "$RED \n-->INSTALLING METASPLOIT<--\n $WHITE"
   pkg install unstable-repo
   pkg install metasploit
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 5 ]
 then
   echo -e "$RED \n-->INSTALLING sploitego<--\n $WHITE"
@@ -61,21 +67,31 @@ then
   echo -e "$RED \n-->COULD NOT FIND<--\n"
   echo -e "try visiting $BLUEhttps://github.com/allfro/sploitego"
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 6 ]
 then
   echo -e "$RED \n-->INSTALLING Hunner<--\n $WHITE"
   git clone https://github.com/b3-v3r/Hunner
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 7 ]
 then
   echo -e "$RED \n-->INSTALLING sqlmap<--\n $WHITE"
   git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
 elif [ $choice == 8 ]
 then
   ehco -e "$RED \n-->INSTALLING shellphish<--\n $WHITE"
   git clone https://github.com/thelinuxchoice/shellphish
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+  tool
+  install
+else
+  echo -e "$BOLDR \n--> ALREADY INSTALLED<--\n"
 fi
 }
 
