7fdf5be1dd0c76256fbccf355ab3cef9e5cc8b35
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 15:22:36 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,33 @@
 
 INSTALL_DIR=/_Tool-Console_
-LOOT_DIR=/usr/share/sniper/loot
-PLUGINS_DIR=/usr/share/sniper/plugins
-GO_DIR=~/go/bin
 
-echo -e "$OKGREEN Type 'YES' to continue$RESET"
+echo -e "$OKGREEN \n-->Type 'yes' to continue<--\n$RESET"
 read answer
 
-echo -e "$OKORANGE Installing Requirement$RESET"
+echo -e "$OKORANGE \n-->Installing Requirement<--\n$RESET"
 apt update && apt upgrade
 https://github.com/ARACADERISE/_Tool-Console_/blob/master/install.sh
 pkg install python
 
-echo -e " -->Stored in $INSTALL_DIR<--$RESET"
+echo -e " \n-->Stored in $INSTALL_DIR<--\n$RESET"
 
-echo -e "$OKORANGE Setting up...$RESET"
+echo -e "$OKORANGE \n-->Setting up...<--\n$RESET"
 git clone https://github.com/ARACADERISE/_Tool-Console_.git
-echo -e "$OKORANGE + -- --=[Setting up Ubuntu$RESET"
+echo -e "$OKORANGE \n-->Setting up Ubuntu<--\n$RESET"
 apt-get update && apt-get upgrade -y
 apt-get install wget -y
 apt-get install proot -y
 apt-get install git -y
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
-echo -e "OKORANGE To use ubuntu \ncd ubuntu-in-termux \nchmod +x ubuntu.sh \n./ubuntu.sh \ncp ~/ubuntu-in-termux/resolv.conf ~/ubuntu-in-termux/ubuntu-fs/etc/
-./start.sh"
+echo -e "\n$OKORANGE To use ubuntu \ncd ubuntu-in-termux \nchmod +x ubuntu.sh \n./ubuntu.sh \ncp ~/ubuntu-in-termux/resolv.conf ~/ubuntu-in-termux/ubuntu-fs/etc/
+./start.sh\n"
 echo -e "$OKORANGE Setting Up Kali...$RESET"
 pkg install wget
 wget https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter
 chmod +x kalinethunter
 bash kalinethunter
 
-echo -e "$OKORANGE + -- --=[Done!$RESET"
-echo -e "$OKORANGE + -- --=[To run, type python file.py! $RESET"
+echo -e "$OKORANGE \n-->Done!<--\n$RESET"
+echo -e "$OKORANGE \n-->To run, type python file.py!<--\n $RESET"
+
+echo -e "\nE  N  J  O  Y\n $RESET"
