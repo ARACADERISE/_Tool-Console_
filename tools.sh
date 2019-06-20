52a658b7dd8fc1740ea50f2719da9d5a97c7fd6f
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-19 19:22:01 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -13,17 +13,22 @@ echo -e "
     (_  _/  \ /  \(  )  ___ / __/  \(  ( / ___)/  \(  ) (  __)
       )((  O (  O / (_/(___( (_(  O /    \___ (  O / (_/\) _) 
      (__)\__/ \__/\___/     \___\__/\_)__(____/\__/\____(____) V.1.0.1
-                   Top Tools, Top Developers, Free,
-                   Tool Download Version
-                   MADE_BY: ARACADE_RISE,
+                 Top Tools, Top Developers, Free,
+                 Tool Download Version
+                 MADE_BY: ARACADE_RISE,
                    "
-echo -e "$BOLDR \n-->NOTE: You can only download one at a time<--\n"                   
+echo -e "$BOLDR \n-->NOTE: You can only download one at a time<--\n"     
+echo -e "$BOLDR \n--> After the tool downloads, type cd, then type ls, then type cd NAME_OF_TOOL and if it is a 
+                      .sh file then bash FILE.sh, and if it a py file you type the version of python it uses, ex python(being python3)
+                      FILENAME.py, or python2(being python 2) FILENAME.py. To have latest version of python installed you must run
+                      bash install.sh<--\n"
 
 echo -e "$RED \n--> Choose A Tool <--\n $WHITE"
 tool()
 {
 echo -e "$BLUE \n-->TOOLS<--\n "
-echo -e "$BLUE\n1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish $WHITE"
+echo -e "$BLUE\n1.Sniper\n2.JohnTheRipper\n3.Hydra\n4.Metasploit\n5.sploitego\n6.Hunner\n7.sqlmap\n8.shellphish\n9.userrecon\n10.BlackEye
+\n11.InstaShell\n12.SayCheese\n13.ClipBoardMe\n14.D-tect\n15.Nmap\n16.RED_HAWK\n17.Crips\n18.OSIF\n$WHITE"
 read -p "Your Choice >> " choice 
 }
 
@@ -90,6 +95,59 @@ then
   echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
   tool
   install
+elif [ $choice == 9 ]
+then
+  echo -e "$RED \n-->INSTALLING userrecon<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/userrecon.git
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 10 ]
+then
+  echo -e "$RED \n-->INSTALLING blackeye<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/blackeye
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 11 ]
+then
+  echo -e "$RED \n-->INSTALLING InstaShell<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/instashell
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 12 ]
+then
+  echo -e "$RED \n-->INSTALLING SayCheese<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/saycheese
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 13 ]
+then
+  echo -e "$RED \n-->INSTALLING ClipBoardMe<--\n $WHITE"
+  git clone https://github.com/thelinuxchoice/clipboardme.git
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 14 ]
+then
+  echo -e "$RED \n-->INSTALLING D-tect<--\n $WHITE"
+  git clone https://github.com/ihamquentin/D-tect.git
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $ choice == 15 ]
+then
+  echo -e "$RED \n-->INSTALLING Nmap<--\n $WHITE"
+  pkg install nmap
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 16 ]
+then
+  echo -e "$RED \n-->INSTALLING RED_HAWK<--\n $WHITE"
+  git clone https://github.com/Tuhinshubhra/RED_HAWK
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 17 ]
+then
+  echo -e "$RED \n-->INSTALLING Crips<--\n $WHITE"
+  git clone https://github.com/Manisso/Crips.git
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
+elif [ $choice == 18 ]
+then
+  echo -e "$RED \n-->INSTALLING OSIF<--\n $WHITE"
+  echo -e "$BOLDR \n-->INSTALLING python2<--\n $WHITE"
+  pkg install git python2
+  echo -e "$BOLDR \n-->NOW INSTALLING OSIF<--\n $WHITE"
+  git clone https://github.com/ciku370/OSIF
+  echo -e "$BOLDR \n--> I  N  S  T  A  L  L  E  D<--\n $WHITE"
 elif [ $choice == 21 ]
 then
   echo -e "$BOLDR \n--> Hope you enjoyed. Hey while you're at it, type in python file.py and checkout the actual project! <--\n"
