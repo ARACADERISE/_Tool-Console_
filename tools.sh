dc0e2a9d1aed6c76830ae33c60d030771f627085
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-21 16:27:40 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,9 +1,10 @@
 clear
 
-
-#command -v ubuntu > /dev/null 2>&1 || { echo >&2  " \n==> I require Ubuntu but it's not installed. Install it <==\n" }
-#command -v kali > /dev/null 2>&1 || { echo >&2  " \n==> I require Kali but it's not installed. Install it. <==\n" }
-
+alert()
+{
+command -v ubuntu > /dev/null 2>&1 || { echo >&2  " \n==> I require Ubuntu but it's not installed. Install it <==\n"; exit 1; }
+command -v kali > /dev/null 2>&1 || { echo >&2  " \n==> I require Kali but it's not installed. Install it. <==\n"; exit 1; }
+}
 
 GREEN="\033[92m"
 RED="\033[91m"
