79680acf168adaaae62264d2f6e96e00922db23e
ARACADERISE <36897041+ARACADERISE@users.noreply.github.com>
2019-06-21 16:22:51 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -22,8 +22,8 @@ echo -e "$GREEN
 }
 title
 
-command -v ubuntu > /dev/null 2>&1 || { echo >&2 $BOLDR " \n==> I require Ubuntu but it's not installed. Install it <==\n" exit 1 }
-command -v kali > /dev/null 2>&1 || { echo >&2 $BOLDR " \n==> I require Kali but it's not installed. Install it. <==\n" exit 1 }
+command -v ubuntu > /dev/null 2>&1 || { echo >&2 $BOLDR " \n==> I require Ubuntu but it's not installed. Install it <==\n" }
+command -v kali > /dev/null 2>&1 || { echo >&2 $BOLDR " \n==> I require Kali but it's not installed. Install it. <==\n" }
 
 info()
 {
